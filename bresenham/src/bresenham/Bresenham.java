0a2b9f78eb7abcb4d42f66c1d763943e8f4fa825
Jorman <tille100@gmail.com>
2012-07-28 00:33:32 -0500

    fixed for created a complete circle with bresenham algorithm

--- bresenham/src/bresenham/Bresenham.java
+++ bresenham/src/bresenham/Bresenham.java
@@ -30,28 +30,32 @@ public class Bresenham extends JPanel {
     Dimension p1 = new Dimension(0,0);
     Dimension p2 = new Dimension(c,0);
     
-    int skip = 10;
+    int skip = 5;
     for( int i = 0; i < w; i+=skip ){
+      g2d.setColor(Color.blue);
       p1.setSize(i,0);
       p2.setSize(w,i);
       bresenham_line(p1,p2,g2d);
 
-      /*p1.setSize(w,i);
-      p2.setSize(i,h);
-      bresenham_line(p1,p2,g2d);*/
+      g2d.setColor(Color.red);
+      p1.setSize(w,i);
+      p2.setSize(w-i,h);
+      bresenham_line(p1,p2,g2d);
       
-      /*p1.setSize(w-i,h);
+      g2d.setColor(Color.black);
+      p1.setSize(w-i,h);
       p2.setSize(0,h-i);
-      bresenham_line(p1,p2,g2d);*/
+      bresenham_line(p1,p2,g2d);
       
-      /*p1.setSize(0,h-i);
+      g2d.setColor(Color.orange);
+      p1.setSize(0,h-i);
       p2.setSize(i,0);
-      bresenham_line(p1,p2,g2d);*/
+      bresenham_line(p1,p2,g2d);
     }
   }
 
   public static void bresenham_line(Dimension p1, Dimension p2, Graphics g2d) {
-
+    
     int y1 = (int) p2.getHeight(), 
         y0 = (int) p1.getHeight(), 
         x1 = (int) p2.getWidth(), 
@@ -60,19 +64,25 @@ public class Bresenham extends JPanel {
         dy =  y1 - y0,
         y = y0,
         x = x0, 
-        incE, incNE, d;
+        incE, incNE, d, stepx, stepy;
+    
+    if (dy < 0){ dy = -dy; stepy = -1; 
+    }else stepy = 1;
+    
+    if (dx < 0){ dx = -dx; stepx = -1; 
+    }else stepx = 1;
     
     if( dx > dy ){
       incE = 2*dy;
       incNE = 2*dy - 2*dx;
       d = 2*dy - dx;
       
-      for( ;x <= x1; ++x ){
+      for( ;x != x1; x+=stepx ){
         g2d.drawLine(x,-(y-h),x,-(y-h));
         if( d < 0 ) d += incE;
         else{
           d += incNE;
-          y++;
+          y+=stepy;
         }
       }
     }else{
@@ -80,12 +90,12 @@ public class Bresenham extends JPanel {
       incNE = 2*dx - 2*dy;
       d = 2*dx - dy;
       
-      for( ;y <= y1; ++y ){
+      for( ;y != y1; y+=stepy ){
         g2d.drawLine(x,-(y-h),x,-(y-h));
         if( d < 0 ) d += incE;
         else{
           d += incNE;
-          x++;
+          x+=stepx;
         }
       }
     }
