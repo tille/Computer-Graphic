4189e850b6ec3b2ef6b2576e863f57e2d9431967
Jorman <tille100@gmail.com>
2012-08-04 18:14:18 -0500

    cohen_sutherland ready, but it's necesary add more design

--- Challenge2/src/cohen_sutherland.java
+++ Challenge2/src/cohen_sutherland.java
@@ -29,19 +29,23 @@ public class Cohen_sutherland extends JPanel {
     g2d.drawLine(right, down, right, top);
   }
   
+  boolean trivial_validations(Dimension p1, Dimension p2){
+    if( p1.width < left  && p2.width < left  ||
+        p1.width > right && p2.width > right ||
+        p1.height < top  && p2.height < top  ||
+        p1.height > down && p2.height > down ) return false;
+    return true;
+    
+  }
+  
   void add_lines(Graphics2D g2d){
-    // La idea esque esto no quede quemado y pueda haber 
-    // una interfaz para agregar nuevas lineas.
     Dimension p1 = new Dimension(50,250);
-    Dimension p2 = new Dimension(350,250);
+    Dimension p2 = new Dimension(250,250);
     g2d.setColor(Color.red);
-    //g2d.drawLine(p1.width, p1.height, p2.width, p2.height);
     
-    /*boolean outside = trivial_validations(p1,p2);
-    if(outside == false) cohen_sutherland(p1,p2, g2d);
-    else g2d.drawLine(p1.width, p1.height, p2.width, p2.height);*/
-    
-    cohen_sutherland(p1,p2, g2d);
+    boolean outside = trivial_validations(p1,p2);
+    if(outside) cohen_sutherland(p1,p2, g2d);
+    else g2d.drawLine(p1.width, p1.height, p2.width, p2.height);
   }
   
   @Override
@@ -67,52 +71,47 @@ public class Cohen_sutherland extends JPanel {
   }
   
   Dimension intersection(Dimension pi, Dimension pf, int i){
-    int x01=0, x11=0, x02=0, x12=0, y01=0, y11=0, y02=0, y12=0;
+
+    int x0 = pi.width;
+    int x1 = pf.width;
+    int y0 = pi.height;
+    int y1 = pf.height;
+    int x=0,y=0;
     
-    x01 = pi.width;
-    x11 = pf.width;
-    y01 = pi.height;
-    y11 = pf.height;
-   
-    if( i == 1 ){ 
-      x02 = left; 
-      y02 = -1;
-      x12 = left; 
-      y12 = 502;
-    }else if( i == 2 ){
-      x02 = right;
-      y02 = -1;
-      x12 = right; 
-      y12 = 502;
+    if (i == 8) {
+      x = (x0 + (down - y0) * (x1 - x0) / (y1 - y0));
+      y = down;
+    } else if (i == 4) {
+      x = (x0 + (top - y0) * (x1 - x0) / (y1 - y0));
+      y = top;
+    } else if (i == 1) {
+      x = left;
+      y = (y0 + (y1 - y0) * (left - x0) / (x1 - x0));
+    } else if (i == 2) {
+      x = right;
+      y = (y0 + (y1 - y0) * (right - x0) / (x1 - x0));
     }
-    
-    double num = (x01-x02-y01+y02)*(y12-y02);
-    double den = ((y11+y01)-(x11-x01))*(x12-x02);
-    double t1 = num/den;
-    
-    double x2 = left;
-    double x = x01+(t1*(x11-x01));
-    double y = y01+(t1*(y11-y01));
-    double y2 = (y01 + (y11 - y01) * (left - x01) / (x11 - x01));
-    
-    
-    Dimension cosa = new Dimension((int)x2,(int)y2);
-    return cosa;
+
+    return new Dimension(x,y);
   }
   
   int find_position(Dimension p){
     if( p.width < left ) return 1;
-    else if ( p.width > right ) return 2;
+    else if( p.width > right ) return 2;
+    else if( p.height < top ) return 4;
+    else if( p.height > down ) return 8;
     return 0;
   }
   
   Dimension step_up(Dimension pi, Dimension pf){
     int c1 = find_position(pi);
-    Dimension result = new Dimension(-360,-1);
+    Dimension result = new Dimension();
     
-    for( int i = 1; result.width == -360; i*=2){
-      if( (c1 & i) > 0 ) result = intersection(pi,pf,i);
-          //System.out.println(".. "+pi.width);
+    for( int i = 1;; i*=2){
+      if( (c1 & i) > 0 ){ 
+        result = intersection(pi,pf,i);
+        break;
+      }
     }
 
     return result;
@@ -121,7 +120,7 @@ public class Cohen_sutherland extends JPanel {
   void cohen_sutherland(Dimension p1, Dimension p2, Graphics2D g2d){
     Dimension cpy1 = p1, cpy2 = p2;
     double total_dist = dist(p1,p2);
-    int error = 0,cont = 0;
+    int error = 0;
     
     while( ( not_is_inside(cpy1) || not_is_inside(cpy2) ) ){
       if( total_dist - (dist(p1,cpy1)+dist(p2,cpy2)) <= 0 ){ 
@@ -129,22 +128,13 @@ public class Cohen_sutherland extends JPanel {
         break;
       }
       
-      if( not_is_inside(cpy1) ){ 
-        /*if( cont < 2 ){
-          //System.out.println("x- "+cpy1.width);
-          //System.out.println("y- "+cpy1.height);
-          cont++;
-        }*/
-        cpy1 = step_up(cpy1,p2);
-      }
-      if( not_is_inside(cpy2) ){ 
-        //System.out.println("cpy2");
-        cpy2 = step_up(cpy2,p1);
-      }
+      if( not_is_inside(cpy1) ) cpy1 = step_up(cpy1,p2);
+      if( not_is_inside(cpy2) ) cpy2 = step_up(cpy2,p1);
     }
     
     if( error == 1 ) g2d.drawLine(p1.width, p1.height, p2.width, p2.height);
     else{
+      g2d.setColor(Color.red);
       g2d.drawLine(p1.width, p1.height, cpy1.width, cpy1.height);
       g2d.setColor(Color.blue);
       g2d.drawLine(cpy1.width, cpy1.height, cpy2.width, cpy2.height);
